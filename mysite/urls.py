e530675c8d09b4ed345be2f74a5839beb99d5c86
chrisheo <jftj@naver.com>
2023-05-31 16:12:06 +0900

    [feat] post에 이미지 필드 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
@@ -7,3 +8,5 @@ urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'', include('blog.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
